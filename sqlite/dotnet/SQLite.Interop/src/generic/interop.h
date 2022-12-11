455988728bd7196085ca85140b583adfab97e3e2
mistachkin <joe@mistachkin.com>
2022-12-11 16:06:40 -0500

    Import the 1.0.117.0 sources into GitHub mirror.

---
+++ sqlite/dotnet/SQLite.Interop/src/generic/interop.h
@@ -0,0 +1,18 @@
+/*
+ * interop.h -
+ *
+ * Written by Joe Mistachkin.
+ * Released to the public domain, use at your own risk!
+ */
+
+#ifndef INTEROP_VERSION
+#define INTEROP_VERSION          "1.0.117.0"
+#endif
+
+#ifndef INTEROP_SOURCE_ID
+#define INTEROP_SOURCE_ID        "2fc62fafbec8615ba33493f72aa27899ab429207"
+#endif
+
+#ifndef INTEROP_SOURCE_TIMESTAMP
+#define INTEROP_SOURCE_TIMESTAMP "2022-11-24 00:38:57 UTC"
+#endif
---
+++ sqlite/dotnet/System.Data.SQLite/SQLitePatchLevel.cs
@@ -0,0 +1,16 @@
+/********************************************************
+ * ADO.NET 2.0 Data Provider for SQLite Version 3.X
+ * Written by Joe Mistachkin (joe@mistachkin.com)
+ *
+ * Released to the public domain, use at your own risk!
+ ********************************************************/
+
+using System.Data.SQLite;
+
+///////////////////////////////////////////////////////////////////////////////
+
+[assembly: AssemblySourceId("2fc62fafbec8615ba33493f72aa27899ab429207")]
+
+///////////////////////////////////////////////////////////////////////////////
+
+[assembly: AssemblySourceTimeStamp("2022-11-24 00:38:57 UTC")]
